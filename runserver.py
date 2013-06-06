b31f82c2f6be7b4e222c32113ffe9288b8b4a3b1
Dirk Dankhoff <dd@robhost.de>
2013-06-06 14:22:31 +0200

    Add runserver command

--- runserver.py
+++ runserver.py
@@ -1,3 +1,21 @@
+from __future__ import print_function
+
+import socket
+import sys
+
 from updian.frontend import app
 
-app.run(debug=False)
+params = dict(debug=False)
+
+try:
+    ip = sys.argv[1]
+    if ip.count('.') != 3:
+        raise socket.error()
+    socket.inet_aton(ip)
+    params['host'] = ip
+except socket.error:
+    print('ERROR: Given IPv4 address is invalid.', file=sys.stderr)
+except IndexError:
+    pass
+
+app.run(**params)
